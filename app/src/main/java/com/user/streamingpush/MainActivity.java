1ba72ebc1eeb0f681f6932d81b0ddd76001b62f8
USR0 <user_wangjianyong@outlook.com>
2017-11-08 20:18:24 +0800

    CameraActivity

--- app/src/main/java/com/user/streamingpush/MainActivity.java
+++ app/src/main/java/com/user/streamingpush/MainActivity.java
@@ -1,12 +1,16 @@
 package com.user.streamingpush;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = "StreamingPush";
+    public static final String TAG = "SP-MainActivity";
+    private Button startbtn;
     RtmpLive rtmplive = new RtmpLive();
 
     @Override
@@ -16,6 +20,13 @@ public class MainActivity extends AppCompatActivity {
 
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
+        startbtn = (Button) findViewById(R.id.btn_start);
+        startbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CameraActivity.class));
+            }
+        });
         tv.setText(rtmplive.stringFromJNI());
         rtmplive.Init("rtmp://video-center.alivecdn.com/live/livestream1?vhost=push.yangxudong.com", new RtmpLive.onStreamingCallback() {
             @Override
@@ -24,6 +35,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-
 }
